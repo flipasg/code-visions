b0dbe913d2bf437b84b2083e451a252b16d9a964
Iker Garcia Ramirez <ikergarciadev@gmail.com>
2023-12-28 18:06:45 +0100

    test(week1): 🎨 update test description

--- week1/flipasg/index.test.ts
+++ week1/flipasg/index.test.ts
@@ -15,7 +15,7 @@ describe('optimizeIntervals', () => {
     expect(optimizeIntervals(intervals)).toEqual(intervals);
   });
 
-  test('should return merged intervals if overlap exist', () => {
+  test('should return merged intervals if overlap exist (one interval is contained in other', () => {
     const intervals: Intervals = [
       [5, 8],
       [2, 7],
@@ -33,7 +33,7 @@ describe('optimizeIntervals', () => {
     expect(optimizeIntervals(intervals)).toEqual([[1, 6], [8, 10]]);
   });
 
-  test('should return merged intervals if overlap exist', () => {
+  test('should return ordered intervals if overlap not exist', () => {
     const intervals: Intervals = [
       [3, 4],
       [1, 2],
