dc30f6ed8ba0a2f75a37c0ce2afa270528444178
Iker Garcia Ramirez <ikergarciadev@gmail.com>
2024-01-13 15:23:43 +0100

    feat(week3): ✨ add enchantment limit

--- week3/flipasg/index.test.ts
+++ week3/flipasg/index.test.ts
@@ -1,30 +1,49 @@
-import { describe, expect, test } from 'vitest';
-import { Weapon } from './types';
+import { afterEach, describe, expect, test, vi } from 'vitest';
+import * as adventure from '.';
+import {
+  dropEnchantment,
+  enchant,
+  enchantWeapon,
+  getEnchantment,
+  roll,
+} from '.';
 import { MAGIC_BOOK, WEAPON } from './constants';
-import { enchant, getRandomEnchantment, roll } from '.';
 
 describe('Enchantment', () => {
-  describe('enchant', () => {
+  const enchantments = Object.values(MAGIC_BOOK);
+
+  afterEach(() => {
+    vi.resetAllMocks();
+  });
+  
+  describe('enchantWeapon', () => {
     test('should enchant weapon with fire', () => {
-      const weapon = WEAPON;
+      const weapon = {
+        name: 'Dagger of the Nooblet',
+        stats: ['5 - 10 attack damage', '1.2 attack speed'],
+      };
+
       const enchanment = MAGIC_BOOK.fire;
       const expectedResult = {
         name: 'Inferno Dagger of the Nooblet',
         stats: ['5 - 10 attack damage', '1.2 attack speed', '+5 fire damage'],
       };
 
-      expect(enchant(weapon, enchanment)).toStrictEqual(expectedResult);
+      expect(enchantWeapon(weapon, enchanment)).toStrictEqual(expectedResult);
     });
 
     test('should enchant weapon with agility', () => {
-      const weapon = WEAPON;
+      const weapon = {
+        name: 'Sword of Destiny',
+        stats: ['35 - 50 attack damage', '1.5 attack speed'],
+      };
       const enchanment = MAGIC_BOOK.agility;
       const expectedResult = {
-        name: 'Quick Dagger of the Nooblet',
-        stats: ['5 - 10 attack damage', '1.2 attack speed', '+5 agility'],
+        name: 'Quick Sword of Destiny',
+        stats: ['35 - 50 attack damage', '1.5 attack speed', '+5 agility'],
       };
 
-      expect(enchant(weapon, enchanment)).toStrictEqual(expectedResult);
+      expect(enchantWeapon(weapon, enchanment)).toStrictEqual(expectedResult);
     });
 
     test('should enchant weapon with lifesteal', () => {
@@ -35,60 +54,140 @@ describe('Enchantment', () => {
         stats: ['5 - 10 attack damage', '1.2 attack speed', '+5 lifesteal'],
       };
 
-      expect(enchant(weapon, enchanment)).toStrictEqual(expectedResult);
+      expect(enchantWeapon(weapon, enchanment)).toStrictEqual(expectedResult);
     });
+  });
 
-    test('should enchant weapon different from Dagger', () => {
+  describe('getEnchantment', () => {
+    test('should return a random enchantment', () => {
+      const results = new Set();
+
+      for (let i = 0; i < 100; i++) {
+        results.add(getEnchantment(enchantments));
+      }
+
+      expect(results.size).toBeGreaterThanOrEqual(enchantments.length);
+    });
+  });
+
+  describe('areEnchantmentsDropped', () => {
+    test('should return true 10% of the time', () => {
+      const rollTimes = 1000;
+      let trueCount = 0;
+
+      for (let i = 0; i < rollTimes; i++) {
+        if (adventure.areEnchantmentsDropped()) {
+          trueCount++;
+        }
+      }
+
+      const expectedTrueCount = rollTimes * 0.1;
+      const tolerance = rollTimes * 0.02; // Allow a 2% tolerance
+
+      expect(Math.abs(trueCount - expectedTrueCount)).toBeLessThanOrEqual(
+        tolerance
+      );
+    });
+  });
+
+  describe('getWeaponEnchantments', () => {
+    test('should return the weapon enchantments', () => {
+      const weapon = { name: enchantments[0].prefix + ' Sword', stats: [] };
+      expect(
+        adventure.getWeaponEnchantments(weapon, enchantments).length
+      ).toBeGreaterThan(0);
+    });
+    test('should return the weapon enchantments (0 if not enchanted)', () => {
+      const weapon = { name: 'Sword', stats: [] };
+      expect(adventure.getWeaponEnchantments(weapon, enchantments).length).toBe(
+        0
+      );
+    });
+  });
+
+  describe('dropEnchantment', () => {
+    test('should drop the enchantment from the weapon', () => {
       const weapon = {
-        name: 'Sword of Destiny',
-        stats: ['35 - 50 attack damage', '1.5 attack speed'],
+        name: 'Inferno Dagger of the Nooblet',
+        stats: ['5 - 10 attack damage', '1.2 attack speed', '+5 fire damage'],
       };
-      const enchanment = MAGIC_BOOK.lifesteal;
+
+      const enchanment = MAGIC_BOOK.fire;
       const expectedResult = {
-        name: 'Vampire Sword of Destiny',
-        stats: ['35 - 50 attack damage', '1.5 attack speed', '+5 lifesteal'],
+        name: 'Dagger of the Nooblet',
+        stats: ['5 - 10 attack damage', '1.2 attack speed'],
       };
 
-      console.log(enchant(weapon, enchanment));
+      expect(dropEnchantment(weapon, enchanment)).toStrictEqual(expectedResult);
+    });
+    test('should return the same weapon if the enchantment is not present ', () => {
+      const weapon = {
+        name: 'Inferno Dagger of the Nooblet',
+        stats: ['5 - 10 attack damage', '1.2 attack speed', '+5 fire damage'],
+      };
 
-      expect(enchant(weapon, enchanment)).toStrictEqual(expectedResult);
+      const enchanment = MAGIC_BOOK.ice;
+
+      expect(dropEnchantment(weapon, enchanment)).toStrictEqual(weapon);
     });
   });
 
-  describe('getRandomEnchantment', () => {
-    test('should return a random enchantment of magic book', () => {
-      const enchantments = Object.values(MAGIC_BOOK);
-      expect(Object.values(MAGIC_BOOK)).toContain(getRandomEnchantment(enchantments));
-      expect(Object.values(MAGIC_BOOK)).toContain(getRandomEnchantment(enchantments));
-      expect(Object.values(MAGIC_BOOK)).toContain(getRandomEnchantment(enchantments));
+  describe('dropEnchantments', () => {
+    test('should drop weapons all enchantments', () => {
+      const weapon = {
+        name: 'Quick Inferno Dagger of the Nooblet',
+        stats: [
+          '5 - 10 attack damage',
+          '1.2 attack speed',
+          '+5 fire damage',
+          '+5 agility',
+        ],
+      };
+
+      const expectedResult = {
+        name: 'Dagger of the Nooblet',
+        stats: ['5 - 10 attack damage', '1.2 attack speed'],
+      };
+
+      expect(adventure.dropEnchantments(weapon, enchantments)).toStrictEqual(
+        expectedResult
+      );
+    });
+
+    test('should return the same weapon if not enchanted', () => {
+      const weapon = {
+        name: 'Dagger of the Nooblet',
+        stats: ['5 - 10 attack damage', '1.2 attack speed'],
+      };
+
+      expect(adventure.dropEnchantments(weapon, enchantments)).toStrictEqual(
+        weapon
+      );
     });
   });
 
-  describe('roll', () => {
+  describe('enchant', () => {
     test('should return a weapon with random enchantment of magic book', () => {
       const weapon = WEAPON;
-      const enchantments = Object.values(MAGIC_BOOK);
+      vi.spyOn(adventure, 'areEnchantmentsDropped').mockReturnValue(false);
 
-      const { name } = roll(weapon);
+      const { name } = enchant(weapon, enchantments, 1);
 
       expect(
         enchantments.some(({ prefix }) => name.startsWith(prefix))
       ).toBeTruthy();
     });
 
-    test('should drop enchantment almost one time', () => {
+    test('should return a weapon with random enchantment of magic book (max 3)', () => {
       let weapon = WEAPON;
-      let dropTimes = 0;
+      const limit = 3;
+      vi.spyOn(adventure, 'areEnchantmentsDropped').mockReturnValue(false);
 
-      for (let index = 0; index < 11; index++) {
-        const {name} = roll(weapon);
-
-        if(name.startsWith('Dagger')){
-          dropTimes++;
-        }
+      for (let i = 0; i < 150; i++) {
+        weapon = enchant(weapon, enchantments, limit);
       }
 
-      expect(dropTimes).toBeGreaterThan(0);
+      expect(weapon.stats.length).toBe(WEAPON.stats.length + limit);
     });
   });
 });
--- week3/flipasg/index.ts
+++ week3/flipasg/index.ts
@@ -1,7 +1,10 @@
 import { MAGIC_BOOK } from './constants';
 import { Enchantment, Weapon } from './types';
 
-export const enchant = (weapon: Weapon, enchantment: Enchantment): Weapon => {
+export const enchantWeapon = (
+  weapon: Weapon,
+  enchantment: Enchantment
+): Weapon => {
   const { name, stats } = weapon;
   const { prefix, attribute } = enchantment;
 
@@ -11,43 +14,113 @@ export const enchant = (weapon: Weapon, enchantment: Enchantment): Weapon => {
   };
 };
 
-export const getRandomEnchantment = (
-  enchantments: Enchantment[]
-): Enchantment => {
+export const getEnchantment = (enchantments: Enchantment[]): Enchantment => {
   const randomNumber = Math.floor(Math.random() * enchantments.length);
 
   return enchantments[randomNumber];
 };
 
-export const roll = (weapon: Weapon) => {
-  const dropPossibilities = Math.floor(Math.random() * 10);
-  const enchantments = Object.values(MAGIC_BOOK);
-
-  let enchanment = getRandomEnchantment(enchantments);
-  const isEnchanted = enchantments.some(({ prefix }) =>
-    weapon.name.startsWith(prefix)
-  );
-  const weaponPrefix = weapon.name.substring(0, weapon.name.indexOf(' '));
-
-  const weaponWithoutEnchantment = {
-    name: weapon.name.substring(weapon.name.indexOf(' ')),
-    stats: [...weapon.stats].slice(0, -1),
-  };
-
-  if(dropPossibilities === 1){
-    return isEnchanted ? weaponWithoutEnchantment : weapon;
-  }
-  while (enchanment.prefix === weaponPrefix) {
-    enchanment = getRandomEnchantment(enchantments);
-  }
-
-  return enchant(isEnchanted ? weaponWithoutEnchantment : weapon, enchanment);
+export const areEnchantmentsDropped = () => {
+  return Math.random() <= 0.1;
 };
 
-export const rollWeapon = (weapon: Weapon, numberOfTimes: number): Weapon => {
+export const getWeaponEnchantments = (
+  weapon: Weapon,
+  enchantments: Enchantment[]
+) => {
+  return enchantments.filter(
+    ({ prefix }) => weapon.name.indexOf(prefix) !== -1
+  );
+};
+
+export const dropEnchantment = (weapon: Weapon, enchantment: Enchantment) => {
+  const name =
+    weapon.name.indexOf(enchantment.prefix) !== -1
+      ? weapon.name.replace(enchantment.prefix + ' ', '')
+      : weapon.name;
+
+    const stats = [...weapon.stats];
+    const indexOfStat = stats.lastIndexOf(enchantment.attribute);
+    if(indexOfStat !== -1){
+      stats.splice(stats.lastIndexOf(enchantment.attribute), 1);
+    }
+  return {
+    name,
+    stats,
+  };
+};
+
+const dropLastEnchantment = (weapon: Weapon, enchanments: Enchantment[]) => {
+
+  for (let index = enchanments.length - 1; index >= 0; index--) {
+    const enchantment = enchanments[index];
+    if(weapon.name.startsWith(enchantment.prefix)){
+      return dropEnchantment(weapon, enchantment);
+    }
+  }
+  return weapon;
+};
+
+export const dropEnchantments = (
+  weapon: Weapon,
+  enchantments: Enchantment[]
+) => {
+  let weaponWithoutEnchantments = weapon;
+
+  enchantments.forEach((enchantment) => {
+    weaponWithoutEnchantments = dropEnchantment(
+      weaponWithoutEnchantments,
+      enchantment
+    );
+  });
+
+  return weaponWithoutEnchantments;
+};
+
+export const enchant = (
+  weapon: Weapon,
+  enchantments: Enchantment[],
+  limit: number
+) => {
+  const weaponEnchantments = getWeaponEnchantments(weapon, enchantments);
+  if (weaponEnchantments.length === 0) {
+    return enchantWeapon(weapon, getEnchantment(enchantments));
+  }
+
+  const allowedEntchantments = enchantments.filter(
+    ({ prefix }) => !weaponEnchantments.find((e) => e.prefix === prefix)
+  );
+  let enchanment = getEnchantment(allowedEntchantments);
+
   let newWeapon = weapon;
-  for (let index = 0; index < 11; index++) {
-    newWeapon = roll(weapon);
+  if (weaponEnchantments.length === limit) {
+    newWeapon = dropLastEnchantment(weapon, enchantments);
+  }
+  return enchantWeapon(newWeapon, enchanment);
+};
+
+
+
+export const roll = (
+  weapon: Weapon,
+  enchantments: Enchantment[],
+  numberOfTimes: number,
+  limit: number
+): Weapon => {
+  let newWeapon = weapon;
+  for (let index = 0; index < numberOfTimes; index++) {
+    newWeapon = !adventure.areEnchantmentsDropped()
+      ? adventure.enchant(newWeapon, enchantments, limit)
+      : adventure.dropEnchantments(newWeapon, enchantments);
   }
   return newWeapon;
 };
+
+const adventure = {
+  enchant,
+  dropEnchantments,
+  areEnchantmentsDropped,
+  roll,
+};
+
+export default adventure;
